0ea2ded5082d06234e1074044c6e64dfb73aa1f3
amshel hack3r <amshelhack3r@gmail.com>
2020-08-28 01:40:39 +0300

    initial commit

---
+++ app/src/main/java/com/example/weatherapp/constants.kt
@@ -0,0 +1,5 @@
+package com.example.weatherapp
+
+const val API_KEY = "692a6255b2b2eec84d28d9e48d325305"
+const val NAIROBI_FORECAST ="https://api.openweathermap.org/data/2.5/weather?q=Nairobi&appid=$API_KEY"
+const val KISERIAN_FORECAST ="https://api.openweathermap.org/data/2.5/weather?lat=-1.426103&lon=36.692732&appid=$API_KEY"
