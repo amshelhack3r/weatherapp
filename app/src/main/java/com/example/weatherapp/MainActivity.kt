62c0fadb1fc6cab325c9cff1c864711cdcd0fde3
amshel hack3r <amshelhack3r@gmail.com>
2020-08-28 01:40:20 +0300

    create a handler to handle message recieved from background thread. Recommended to switch to coroutines

--- app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -2,10 +2,27 @@ package com.example.weatherapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.os.Message
+import com.example.weatherapp.model.Forecast
+import kotlinx.android.synthetic.main.fragment_city.*
 
 class MainActivity : AppCompatActivity() {
+
+    val handler = object:Handler(Looper.getMainLooper()){
+        override fun handleMessage(msg:Message){
+            val forecast = msg.obj as Forecast
+            println(forecast.city)
+            cv_city_name.text = forecast.city
+        }
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.fragment_city)
+
+        MyThread(handler).start()
     }
+
+
 }
