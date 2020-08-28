59808e60148c5ab75769685626f37a6b93e70344
amshel hack3r <amshelhack3r@gmail.com>
2020-08-28 16:28:01 +0300

    Add apikey and sharedpreferences name to constants

---
+++ app/src/main/java/com/example/weatherapp/util/constants.kt
@@ -0,0 +1,5 @@
+package com.example.weatherapp.util
+
+const val API_KEY = "692a6255b2b2eec84d28d9e48d325305"
+const val BASE_URL = "https://api.openweathermap.org/data/2.5/weather?appid=$API_KEY"
+const val SHARED_PREF = "worldweather"
