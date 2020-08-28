9afc26697cec18eff6cad01d1eae853d0d11514f
amshel hack3r <amshelhack3r@gmail.com>
2020-08-28 23:00:09 +0300

    remove redundant comments

--- app/src/main/java/com/example/weatherapp/fragment/CityFragment.kt
+++ app/src/main/java/com/example/weatherapp/fragment/CityFragment.kt
@@ -122,15 +122,7 @@ class CityFragment : Fragment(), View.OnClickListener {
         transaction.commit()
     }
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment cityFragment.
-         */
-        // TODO: Rename and change types and number of parameters
+
         @JvmStatic
         fun newInstance(param1: String) =
             CityFragment().apply {
