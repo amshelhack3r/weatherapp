3d19a48a898d5a61e3e8e6df8634156e000dc8a5
amshel hack3r <amshelhack3r@gmail.com>
2020-08-28 22:11:46 +0300

    add webview url

--- app/src/main/java/com/example/weatherapp/util/constants.kt
+++ app/src/main/java/com/example/weatherapp/util/constants.kt
@@ -2,4 +2,6 @@ package com.example.weatherapp.util
 
 const val API_KEY = "692a6255b2b2eec84d28d9e48d325305"
 const val BASE_URL = "https://api.openweathermap.org/data/2.5/weather?appid=$API_KEY"
+const val FIVE_DAY ="https://api.openweathermap.org/data/2.5/forecast?appid=$API_KEY"
 const val SHARED_PREF = "worldweather"
+const val webview_url ="https://mutall.co.ke/webview/"
